c626b00e7863c4fa569e32b1a4eeed2de94e0214
reifer <reinaldofsjunior@gmail.com>
2024-05-17 20:41:42 -0300

    Exercícios do desafio de código.

---
+++ BootcampPython/DesafioCodigo/plano_internet.py
@@ -0,0 +1,48 @@
+# TODO: Crie uma Função: recomendar_plano para receber o consumo médio mensal:
+
+
+# TODO: Crie uma Estrutura Condicional para verifica o consumo médio mensal
+# TODO: Retorne o plano de internet adequado:
+
+
+# Solicita ao usuário que insira o consumo médio mensal de dados:
+consumo = float(input("Digite o consume médio mensal de dados em GB: "))
+# Chama a função recomendar_plano com o consumo inserido e imprime o plano recomendado:
+
+def recomendar_plano(consumo):
+    if consumo >= 1 and consumo <= 10:
+        return "Plano Essencial Fibra - 50Mbps"
+    elif consumo > 10 and consumo <= 20:
+        return "Plano Prata Fibra - 100Mbps"
+    elif consumo > 20:
+        return "Plano Premium Fibra - 300Mbps"
+    else:
+        return "Opção inválida. Verifique o valor de dados."
+
+print(recomendar_plano(consumo))
+
+# Desafio
+# Uma empresa de telecomunicações deseja criar uma solução algorítmica que ajude aos seus clientes a escolherem o plano de internet ideal com base em seu consumo mensal de dados. Para a resolução, você pode solicitar ao usuário que insira o seu consumo, sendo este um valor 'float'. Crie uma função chamada recomendar_plano para receber o consumo médio mensal de dados informado pelo cliente, além de utilizar estruturas condicionais para fazer a verificação e retornar o plano adequado.
+#
+# Planos Oferecidos:
+#
+# - Plano Essencial Fibra - 50Mbps: Recomendado para um consumo médio de até 10 GB.
+# - Plano Prata Fibra - 100Mbps: Recomendado para um consumo médio acima de 10 GB até 20 GB.
+# - Plano Premium Fibra - 300Mbps: Recomendado para um consumo médio acima de 20 GB.
+#
+# Entrada
+# Como entrada solicite o consumo médio mensal de dados em GB (float).
+#
+# Saída
+# Retorne o plano ideal para o cliente de acordo com o consumo informado na entrada.
+# def menu():
+#     menu = """\n
+#     ================ MENU ================
+#     [e]\tPlano Essencial - consumo médio de até 10 GB
+#     [p]\tPlano Prata - consumo médio acima de 10 GB até 20 GB.
+#     [pm] Plano Premium - consumo médio acima de 20 GB.
+#     [q]\tSair
+#     => """
+#     return input(textwrap.dedent(menu))
+#
+# while
